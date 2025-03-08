10b9b1d9dfccfbd82bed69c02cf68e8ee1973cd5
You Song Hou <ysongweb3@gmail.com>
2025-03-08 15:22:42 -0500

    chore: update hardhat.config.js to deploy contracts on Mantle Sepolia Testnet

--- hardhat/aiAgent/index.js
+++ hardhat/aiAgent/index.js
@@ -1,7 +1,11 @@
 const createStoryGameAgent = require('./storyGameAgent');
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const providerUrl = "http://127.0.0.1:8545/";
+// const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// const providerUrl = "http://127.0.0.1:8545/";
+
+const contractAddress = "0xBdf3a2c90cEAB5A307e78956Bc1BeF33c19C1F78";
+const providerUrl = "https://rpc.sepolia.mantle.xyz";
+
 
 const agent = createStoryGameAgent(contractAddress, providerUrl);
 
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    mantleSepolia: {
+      url: "https://rpc.sepolia.mantle.xyz", // Sepolia Testnet
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+  },
 };
